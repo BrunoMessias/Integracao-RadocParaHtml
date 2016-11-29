644ad4e2c724b5fecf356356af810b8deef66c8b
TiagoDamascena <tiagobdamascena@gmail.com>
2016-11-29 21:54:08 -0200

    Segunda versao do conversor

--- src/main/webapp/js/radoc-builder.js
+++ src/main/webapp/js/radoc-builder.js
@@ -4,26 +4,44 @@ $(function() {
 
 function loadRelatos(json) {
         $.getJSON(json, function (radoc) {
+            $('#ano-base').text(radoc.anoBase);
+
             var relatorio = $('#relatorio');
-            relatorio.append(
-                $(document.createElement('h1')).text(radoc.anoBase)
-            );
+
             $.each(radoc.relatos, function (key, value) {
-                relatorio.append(
-                    $(document.createElement('div')).addClass('relato').attr('id', 'relato-'+key)
-                );
-                var relato = $('#relato-'+key);
+                var repetido = false;
+                var relato;
+                $('[id^="relato-"]').each(function () {
+                    if(value.classe === $(this).find('.cabecalho-relato a').text()) {
+                        relato = $(this);
+                        repetido = true;
+                        return false;
+                    }
+                });
+
+                if(!repetido) {
+                    relatorio.append(
+                        $(document.createElement('div')).addClass('row relato').attr('id', 'relato-'+key)
+                    );
+                    relato = $('#relato-'+key);
+                    relato.append(
+                        $(document.createElement('div')).addClass('cabecalho-relato').append(
+                            $(document.createElement('a')).text(value.classe)
+                        )
+                    );
+                }
+
                 relato.append(
-                    $(document.createElement('div')).addClass('cabecalho').append(
-                        $(document.createElement('h3')).text(value.classe)
-                    ),
-                    $(document.createElement('div')).addClass('conteudo')
+                    $(document.createElement('div')).addClass('conteudo-relato').attr('id', 'conteudo-'+key)
                 );
-                var conteudo = relato.find('.conteudo');
+
+                var conteudo = relato.find('#conteudo-'+key);
                 $.each(value.atributos, function (key, value) {
                     conteudo.append(
-                        $(document.createElement('a')).text(key +': '+ value),
-                        $(document.createElement('br'))
+                        $(document.createElement('div')).addClass('atributo').append(
+                            $(document.createElement('div')).addClass('key-atributo').text(key+':'),
+                            $(document.createElement('div')).addClass('key-value').text(value)
+                        )
                     );
                 });
             });
